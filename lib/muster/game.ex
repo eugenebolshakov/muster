2de488046b064cda5b9d6eb3adc63b847b2bfe73
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2021-03-06 10:31:24 +0000

    Place tile of value 2 in a random space when starting a game
    When a game is started, the grid must be empty except for one tile of
    value 2 placed at random.
    
    The test creates a new game 5 time to verify that the tile is not placed
    in the same location every time. 5 is random number and should be
    increased if the test proves to be flaky.

--- lib/muster/game.ex
+++ lib/muster/game.ex
@@ -1,19 +1,45 @@
 defmodule Muster.Game do
   @type t :: %__MODULE__{
-          grid: [Muster.rows()]
+          grid: [Muster.row()]
         }
 
   @enforce_keys [:grid]
   defstruct [:grid]
 
   @grid_size 6
+  @first_tile 2
 
+  @spec new() :: t()
   def new() do
     grid =
       nil
       |> List.duplicate(@grid_size)
       |> List.duplicate(@grid_size)
+      |> put_tile_in_random_space(@first_tile)
 
     %__MODULE__{grid: grid}
+  end
+
+  defp put_tile_in_random_space(grid, tile) do
+    random_index =
+      grid
+      |> list_spaces()
+      |> Enum.random()
+
+    put_tile_at(grid, tile, random_index)
+  end
+
+  defp list_spaces(grid) do
+    grid
+    |> List.flatten()
+    |> Enum.with_index()
+    |> Enum.filter(fn {cell, _i} -> is_nil(cell) end)
+    |> Enum.map(fn {nil, i} -> i end)
+  end
+
+  defp put_tile_at(grid, tile, index) do
+    i = div(index, @grid_size)
+    j = Integer.mod(index, @grid_size)
+    put_in(grid, [Access.at(i), Access.at(j)], tile)
   end
 end
--- test/muster/game_test.exs
+++ test/muster/game_test.exs
@@ -5,16 +5,34 @@ defmodule Muster.GameTest do
 
   describe "new/0" do
     test "returns a Game with a 6x6 grid" do
-      game = Game.new()
+      %{grid: grid} = Game.new()
 
-      assert game.grid == [
-               [nil, nil, nil, nil, nil, nil],
-               [nil, nil, nil, nil, nil, nil],
-               [nil, nil, nil, nil, nil, nil],
-               [nil, nil, nil, nil, nil, nil],
-               [nil, nil, nil, nil, nil, nil],
-               [nil, nil, nil, nil, nil, nil]
-             ]
+      assert length(grid) == 6
+
+      grid
+      |> Enum.with_index()
+      |> Enum.each(fn {row, i} ->
+        assert length(row) == 6, "Row #{i} has wrong length"
+      end)
+    end
+
+    test "places a tile of value 2 in a random space" do
+      tile_locations =
+        0..5
+        |> Enum.map(fn _ ->
+          %{grid: grid} = Game.new()
+
+          elements = List.flatten(grid)
+          assert length(elements) == 36
+
+          assert Enum.count(elements, &(&1 == 2)) == 1
+          assert Enum.count(elements, &is_nil/1) == 35
+
+          Enum.find_index(elements, &(&1 == 2))
+        end)
+        |> Enum.uniq()
+
+      assert length(tile_locations) > 1, "Tile placed in the same location 5 times"
     end
   end
 end
