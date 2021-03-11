fbc74aec8fda4078aeab842a540f561d79f87a60
Eugene Bolshakov <eugene.bolshakov@gmail.com>
2021-03-11 11:04:56 +0000

    Make the UI less ugly
    * Add templates for different game states to avoid one huge template.
    * Use CSS grid layout for the game grid which is a bit cleaner than
    flexbox.
    * Tweak styling of headers, buttons, etc to make things look less ugly.
    * Always display current player name for clarity.

--- apps/muster_web/lib/muster_web/views/game_view.ex
+++ apps/muster_web/lib/muster_web/views/game_view.ex
@@ -1,3 +1,11 @@
 defmodule MusterWeb.GameView do
   use MusterWeb, :view
+
+  def player_name(player, current_player) do
+    if player == current_player do
+      "You"
+    else
+      player
+    end
+  end
 end
